9b8236f6451fb704220ff9d78ddf6ec11c001b92
Jabez Borja <jabez.natsu@gmail.com>
2022-01-29 08:20:57 +0800

    add: more info

--- build/src/main.py
+++ build/src/main.py
@@ -16,5 +16,23 @@ class MyWebpage(Container):
                     className = "mt-3 text-lg",
                     textContent = "You're now seeing a PyFyre app running on the wild. Python? Running? On the client-side web!!!!??"
                 ),
+                Container(
+                    className="flex justify-center items-center",
+                    children=[
+                        Text(
+                            className = "mt-3 text-lg",
+                            textContent = "Sounds like replacing Javascript on the web. Anyways, wanna try PyFyre out?"
+                        ),
+                        Anchor(
+                            className="text-sm mt-3 ml-2 font-bold",
+                            textContent="https://github.com/pyfyre/pyfyre",
+                            link="https://github.com/pyfyre/pyfyre"
+                        )
+                    ]
+                ),
+                Text(
+                    className = "mt-10 mx-auto rounded-lg py-2 w-2/12 text-xl font-bold bg-[#333333]",
+                    textContent = "pip install pyfyre"
+                ),
             ]
         )
