93d4eee273dfc7becc43b3c00be3006f8e310af2
Jabez Borja <jabez.natsu@gmail.com>
2022-01-29 08:01:45 +0800

    add: design

--- build/src/main.py
+++ build/src/main.py
@@ -3,17 +3,18 @@ from pyf_modules.widgets import *
 # You can make components by inherting a Widget class
 # and initialize it on the `super().__init__()`.
 class MyWebpage(Container):
+    
     def __init__(self):
         super().__init__(
-            className = "container",
+            className = "h-screen w-screen flex flex-col justify-center text-center text-white font-mono",
             children = [
                 Text(
-                    className = "title",
-                    textContent = "Welcome to PyFyre!"
+                    className = "text-6xl font-bold",
+                    textContent = "Welcome to PyFyre Test Website!"
                 ),
                 Text(
-                    className = "desc",
-                    textContent = "Try to experiment, edit the `src/main.py` and wait for it to reload automatically on a liveserver."
-                )
+                    className = "mt-3 text-lg",
+                    textContent = "You're now seeing a PyFyre app running on the wild. Python? Running? On the client-side web!!!!??"
+                ),
             ]
         )
